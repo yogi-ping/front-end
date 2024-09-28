4ba19d55030a520e7437b2b1d6e77d73920e4dd5
leechan <dlcks0601@gmail.com>
2024-09-29 00:35:14 +0900

    chore: home, login 폴더 정리 후 selectplace 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from 'react-router-dom';
-import Header from './components/Header'; // Header 컴포넌트 올바르게 import
-import Contents from './components/home/Contents';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'; // Home 컴포넌트 import
 import { Plan } from './pages/Plan';
-import Login from './components/login/Login'; // Login 컴포넌트 import
+import Login from './pages/Login'; // Login 컴포넌트 import
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <MainLayout />
+      <Routes>
+        {/* 기본 경로에 Home 컴포넌트를 보여줌 */}
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/plan' element={<Plan />} />
+      </Routes>
     </Router>
-  );
-}
-
-function MainLayout() {
-  const location = useLocation();
-
-  return (
-    <div>
-      {/* 로그인 페이지가 아닐 때만 Header 렌더링 */}
-      {location.pathname !== '/login' && <Header />}
-
-      <main>
-        <Routes>
-          {/* 기본 경로에 Contents를 보여줌 */}
-          <Route path='/' element={<Contents />} />
-          {/* 로그인 페이지로 이동 */}
-          <Route path='/login' element={<Login />} />
-          <Route path='/plan' element={<Plan/>}/>
-        </Routes>
-      </main>
-    </div>
   );
 }
 
