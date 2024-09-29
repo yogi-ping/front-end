2677a62f053c7ab650cada8f6103ba2685cb00d4
leechan <dlcks0601@gmail.com>
2024-09-29 15:01:11 +0900

    feat: 장소선택, 검색바 (재사용 가능하게 common에 SearchInput )

--- src/components/home/Contents.tsx
+++ src/components/home/Contents.tsx
@@ -1,22 +1,25 @@
-// home/Contents.tsx
 import { useState } from 'react';
 import Title from './Title'; // 헤더 컴포넌트
 import SearchBar from './SearchBar'; // 검색바 컴포넌트
 import TravelDestinationBox from './TravelDestinationBox'; // 위치 리스트 관리
 
 export const Contents = (): JSX.Element => {
-  const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
-  return (
-    <div className='flex flex-col w-full max-w-screen-xl mx-auto h-auto items-center justify-center gap-12 relative mt-16 transform scale-110'>
-      {/* 컨텐츠 상단에 여백 추가 */}
-      <div className='flex flex-col items-center gap-12 w-full'>
-        <Title />
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <TravelDestinationBox />
-      </div>
-    </div>
-  );
+    return (
+        <div className='flex flex-col w-full max-w-screen-xl mx-auto h-auto items-center justify-center gap-12 relative mt-16 transform scale-110'>
+            {/* 컨텐츠 상단에 여백 추가 */}
+            <div className='flex flex-col items-center gap-12 w-full'>
+                <Title />
+                {/* 여기서 SearchBar 크기 조정 가능 */}
+                <SearchBar
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                />
+                <TravelDestinationBox />
+            </div>
+        </div>
+    );
 };
 
 export default Contents;
--- src/components/plan/selectplace/SelectPlace.tsx
+++ src/components/plan/selectplace/SelectPlace.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { FaCheck, FaPlus } from 'react-icons/fa';
 
-function SelectPlace() {
-  return <div>SelectPlace</div>;
+interface SelectPlaceProps {
+    isSelected: boolean;
+    onSelect: () => void;
 }
+
+const SelectPlace: React.FC<SelectPlaceProps> = ({ isSelected, onSelect }) => {
+    return (
+        <div className='flex items-center' onClick={onSelect}>
+            {isSelected ? (
+                <FaCheck className='text-blue-500' size={24} /> // 선택된 경우 체크 아이콘
+            ) : (
+                <FaPlus className='text-gray-500' size={24} /> // 선택되지 않은 경우 플러스 아이콘
+            )}
+        </div>
+    );
+};
 
 export default SelectPlace;
