a5dc6e28f42f95aa89162dddebda046cdb1664f5
HongbiKo <rhghdql666@gmail.com>
2024-10-01 01:44:04 +0900

    fix: plan페이지 사이드메뉴 css수정

--- src/components/common/Avartar.tsx
+++ src/components/common/Avartar.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
+import { FaRegUser } from "react-icons/fa6";
 
 export const Avartar:React.FC = () => {
   const [name, setName] = useState('이찬');
   return(
     <div className='w-full max-w-24 flex flex-col items-center cursor-pointer'>
-      <div className='size-20 rounded-full bg-slate-200 flex justify-center items-center'>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10"><path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-        </svg>
+      <div className='size-[4.3rem] rounded-full bg-slate-200 flex justify-center items-center'>
+      <FaRegUser size={31}/>
       </div>
-      <span>{name}</span>
+      <span className='mt-2 text-sm'>{name}</span>
     </div>
   );
 
--- src/components/plan/sidemenu/IndexSidemenu.tsx
+++ src/components/plan/sidemenu/IndexSidemenu.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+
 import { Logo } from './Logo';
 import { TravelDays } from './TravelDays';
 import { Friends } from './Friends';
 
 export const IndexSidemenu:React.FC = () => {
   return(
-    <div className = "w-1/6 flex flex-col items-center">
+    <div className = "w-40 flex flex-col items-center">
       <Logo />
       <TravelDays />
       <Friends />
