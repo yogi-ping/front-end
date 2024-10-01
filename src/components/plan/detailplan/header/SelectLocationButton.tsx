314815ca685c1824e0f41541ce709f4408076bfe
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-01 17:14:56 +0900

    refactor : 여행 장소 선택 토글버튼 분리

---
+++ src/components/plan/detailplan/header/SelectLocationButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+interface SelectLocationButtonProps {
+  isAddingLocation: boolean;
+  setIsAddingLocation: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SelectLocationButton: React.FC<SelectLocationButtonProps> = ({
+  isAddingLocation,
+  setIsAddingLocation,
+}) => {
+  return (
+    <button
+      className={`w-[50px] h-[30px] ${
+        isAddingLocation ? 'bg-[#fed766]' : 'bg-[#ABABAB]'
+      } rounded-[100px] px-[2px] `}
+      onClick={() => setIsAddingLocation(!isAddingLocation)}
+      aria-label="addLocationButton"
+    >
+      <div
+        className={`w-[27px] h-[27px] bg-white rounded-full transition-transform duration-300 ${
+          isAddingLocation ? 'translate-x-[19px]' : 'translate-x-0'
+        }`}
+      />
+    </button>
+  );
+};
+
+export default SelectLocationButton;
