260eebbf1d06415301e029a770601405e721a0b4
youjeans-s <zzinzzin1234@naver.com>
2024-10-01 11:39:47 +0900

    test(calendar): 화면에서 보이는 테스트

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,6 +1,7 @@
 import { Header } from '../components/plan/detailplan/Header';
 import { IndexSidemenu } from '../components/plan/sidemenu/IndexSidemenu';
 import { PlaceList } from '../components/plan/selectplace/PlaceList';
+import WeatherDateRangePicker from '../components/plan/calendar/WeatherDateRangePicker';
 
 export const Plan: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ export const Plan: React.FC = () => {
       <IndexSidemenu />
       <Header />
       <PlaceList />
+      {/* <WeatherDateRangePicker/> */}
     </div>
   );
 };
