6e28ccf3014af8a23fe4cc412cf0a7793de62406
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-07 23:41:38 +0900

    refactor : 여행기간에 대한 컴포넌트 추상화

--- src/components/plan/detailplan/header/Header.tsx
+++ src/components/plan/detailplan/header/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Calendar from '../../../../assets/Calendar.svg';
 import TravelDestionationName from './TravelDestionaionName';
 import SelectLocationButton from './SelectLocationButton';
+import TravelTerm from './TravelTerm';
 
 export const Header: React.FC = () => {
   const [travelDestination, setTravelDestination] = useState<string>('제주');
@@ -10,22 +11,19 @@ export const Header: React.FC = () => {
   );
   const [isAddingLocation, setIsAddingLocation] = useState<boolean>(false);
   return (
-    <div className="w-[400px] flex flex-col px-[35px] py-[20px]">
+    <>
       <div className="flex justify-between mb-[15px]">
         <TravelDestionationName travelDestination={travelDestination} />
         <div className="flex justify-center items-center">
           <p className="text-[20px] font-semibold mr-[5px]">장소추가</p>
           <SelectLocationButton
             isAddingLocation={isAddingLocation}
-            setIsAddingLocation={setIsAddingLocation} // prop 전달
+            setIsAddingLocation={setIsAddingLocation}
           />
         </div>
       </div>
 
-      <div className="flex justify-start items-center">
-        <p className="text-[18px] font-semibold mr-[10px]">{travelTerm}</p>
-        <img src={Calendar} className="w-[27px] h-[27px]" />
-      </div>
-    </div>
+      <TravelTerm travelTerm={travelTerm} />
+    </>
   );
 };
