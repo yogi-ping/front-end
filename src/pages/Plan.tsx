0a6a9fc09f2c9c98da7c48c18174797d23eaf231
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-07 23:46:22 +0900

    chore : Header -> DetailPlan 으로 컴포넌트이름 수정

--- src/pages/Plan.tsx
+++ src/pages/Plan.tsx
@@ -1,15 +1,15 @@
-import { Header } from '../components/plan/detailplan/header/Header';
 import { IndexSidemenu } from '../components/plan/sidemenu/IndexSidemenu';
 import { PlaceList } from '../components/plan/selectplace/PlaceList';
+import DetailPlan from '../components/plan/detailplan/index';
 import WeatherDateRangePicker from '../components/plan/calendar/WeatherDateRangePicker';
 
 export const Plan: React.FC = () => {
   return (
-    <div className='w-full flex'>
+    <div className="w-full flex">
       <IndexSidemenu />
-      <Header />
+      <DetailPlan />
       <PlaceList />
-      {/* <WeatherDateRangePicker/> */}
+      {/* <WeatherDateRangePicker /> */}
     </div>
   );
 };
