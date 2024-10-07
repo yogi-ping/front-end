0b77eb3b2d3f94a9df28bef60039aed230252f83
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2024-10-07 23:45:03 +0900

    refactor : 여행 몇 일차 컴포넌트 분리 및 재사용성 개선

---
+++ src/components/plan/detailplan/planlist/Time.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Time = () => {
+  return (
+    <div className="flex mb-[16px]">
+      <p className="text-[15px] font-semibold mr-[134px]">1일차</p>
+      <p className="text-[13px] text-[#8B8B8B] font-semibold">2024-09-04</p>
+    </div>
+  );
+};
+
+export default Time;
